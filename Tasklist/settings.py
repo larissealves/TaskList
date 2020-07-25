f7db236ce6c9debd0db8e65aeaad0f3aa59379c2
Larisse Alves <alves.larisser@gmail.com>
2020-07-25 17:48:47 -0300

    --

--- Tasklist/settings.py
+++ Tasklist/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'Tasklist.urls'
 
 TEMPLATES = [
