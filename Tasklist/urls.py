cc37618972e1c574984336012ba7c931432134a3
Larisse Alves <alves.larisser@gmail.com>
2020-07-26 13:02:08 -0300

    --

--- Tasklist/urls.py
+++ Tasklist/urls.py
@@ -22,4 +22,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('AppTaskList.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL)
