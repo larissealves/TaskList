d9d9967ea05959621e89118009984752711cee88
Larisse Alves <alves.larisser@gmail.com>
2020-07-26 12:46:08 -0300

    --

--- Tasklist/settings.py
+++ Tasklist/settings.py
@@ -128,10 +128,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'css')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "css")
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
