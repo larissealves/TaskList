6544d940790dfbcce4538238eb83a3e32ced3c27
Larisse Alves <alves.larisser@gmail.com>
2020-07-26 13:43:50 -0300

    --

--- AppTaskList/urls.py
+++ AppTaskList/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('',views.index, name="list"),
@@ -9,4 +8,4 @@ urlpatterns = [
     path('deleteTask/<str:pk>/', views.deleteTask, name="delete"),
     path('description/',views.createDescriptions, name="create_description"),
     path('details/<str:pk>/', views.details, name="details"),
-]+ static(settings.STATIC_URL)
+]
