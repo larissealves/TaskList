dbeb20d91a2fc14717e196dec406ca98243f1a81
Larisse Alves <alves.larisser@gmail.com>
2020-07-29 14:01:36 -0300

    --

--- AppTaskList/urls.py
+++ AppTaskList/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('',views.index, name="list"),
     path('updateTask/<str:pk>/', views.updateTask, name="update_task"),
     path('deleteTask/<str:pk>/', views.deleteTask, name="delete"),
-    path('description/',views.createDescriptions, name="create_description"),
+    #path('description/',views.createDescriptions, name="create_description"),
     path('details/<str:pk>/', views.details, name="details"),
     #path('description/<int:pk>', views.createDescriptions, name="description")
 ]
